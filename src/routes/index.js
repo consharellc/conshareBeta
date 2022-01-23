53684f46ba6069857012977539c72733679c3f47
dom-inic <dominicnyambane22@gmail.com>
2022-01-23 23:19:24 +0300

    main, routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ const App = ({match}) => (
   <div className="gx-main-content-wrapper">
     <Switch>
       <Route path={`${match.url}sample`} component={asyncComponent(() => import('./SamplePage'))}/>
+      <Route path={`${match.url}home`} component={asyncComponent(() => import('./main'))}/>
     </Switch>
   </div>
 );
