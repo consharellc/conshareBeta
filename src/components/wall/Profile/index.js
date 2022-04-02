baec1ce9b033a1205e5698bc33612b47004d1a8d
dom-inic <dominicnyambane22@gmail.com>
2022-04-02 22:49:26 +0300

    home

--- src/components/wall/Profile/index.js
+++ src/components/wall/Profile/index.js
@@ -2,10 +2,12 @@ import React, {useState} from "react";
 import {Button} from "antd";
 import {useSelector} from "react-redux";
 import Auxiliary from "util/Auxiliary";
+import {useAuth} from '../../../authentication/';
 
 const Profile = (props) => {
 
   const [isFollow, setIsFollow] = useState(false);
+  const {authUser} = useAuth();
 
   // const authUser = useSelector(({auth}) => auth.authUser);
 
@@ -22,22 +24,22 @@ const Profile = (props) => {
     <Auxiliary>
       <div className="gx-profileon">
         <div className="gx-profileon-thumb gx-profileon-thumb-htctrcrop">
-          <img src={image} alt=''/>
+          <img src={authUser.profile_pic} alt=''/>
         </div>
         <div className="gx-profileon-content">
-          <p className="gx-profileon-title">{name}</p>
-          <span className="gx-fs-sm">{address}</span>
+          <p className="gx-profileon-title">{authUser.name || authUser.username}</p>
+          <span className="gx-fs-sm">{authUser.city} {authUser.country} </span>
         </div>
       </div>
 
       <div className="gx-follower gx-text-center">
         <ul className="gx-follower-list">
           <li>
-            <span className="gx-follower-title">{followers}</span>
+            <span className="gx-follower-title">{authUser.followers_count}</span>
             <span>Followers</span>
           </li>
           <li>
-            <span className="gx-follower-title">{following}</span>
+            <span className="gx-follower-title">{authUser.followers_count}</span>
             <span>Following</span>
           </li>
           <li>
@@ -47,7 +49,7 @@ const Profile = (props) => {
         </ul>
       </div>
       <div className="gx-mb-xl-4 gx-mb-3">
-        <p>You are following {name}</p>
+        <p>You are following {authUser.username}</p>
         {/* {authUser === id ? null :
           <Button className="gx-btn-sm gx-mb-0" type="primary"
                   onClick={handleToggle}>{isFollow === true ? 'Follow' : 'Unfollow'}</Button>
